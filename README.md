a455b75a24e9661a97f0b06c91e8f62ac36f2106
Angelo Burzi <angelo.burzi@immobiliare.it>
2022-11-28 16:55:49 +0100

    Created

---
+++ README.md
@@ -0,0 +1,128 @@
+# microtunnel-server
+
+**microtunnel-server** is an [express](https://www.npmjs.com/package/express) based library to enable post-quantum protected communication between apps (it must be used with [microtunnel-client](https://www.npmjs.com/package/microtunnel-client)). You can send any JSON-serializable data. It also uses [supersphincs](https://www.npmjs.com/package/supersphincs) for app authentication, [kyber-crystals](https://www.npmjs.com/package/kyber-crystals) for asymmetric encryption and [symcryptor](https://www.npmjs.com/package/symcryptor) for symmetric encryption.
+
+## Installation
+
+```bash
+npm i microtunnel-server
+```
+
+## Usage
+
+First, you have to create app credentials and save them in a json file. Enter in module path typing `cd path-to-your-project/node-modules/microtunnel-server` and then run `npm run -s cred-generate > ../../appCred.json` to save the credentials in your project root.
+
+```javascript
+// appCred.json
+{
+    // Supersphincs keys base-64 encoded
+    "publicKey": "X67kzs9zrKfbayvF5SIsulZzfUYHeTm6BoFTD/BWiryIcOWcaR8d6M4LpaOylCi4DqY59ABNt1nNnfFZjG4akE4hcKaMyx5ar9Uds2Op687uecLGWb0n6W+voSDKzMS8",
+    "privateKey": "A7sP+3n8KCPgXw7VjziPHZHyDL3eavr6iRn1ampyONlfruTOz3Osp9trK8XlIiy6VnN9Rgd5OboGgVMP8FaKvKggCD7A59Lp4M3LaA9XQi8P+SppMxTmapwjfKVJMacSA0fQnqLZ2m/MP3/YcnyG1TH+RFyEM4O/fE7kxB1/fF+IcOWcaR8d6M4LpaOylCi4DqY59ABNt1nNnfFZjG4akE4hcKaMyx5ar9Uds2Op687uecLGWb0n6W+voSDKzMS8",
+
+    // Random bytes base-64 encoded for ID
+    "agent": "285gWsTqj3Gza+3AxJn1qrWzAvf/Lf5i"
+}
+```
+
+Then you have to create a JSON file containing clients info. In this example we have two apps named `frontEnd` and `sessions`:
+
+```javascript
+// authClients.json
+{
+    "frontEnd": {
+        // Client IP
+        "ip": "127.0.0.1",
+
+        // The Agent ID of the App
+        "agent": "TvjXC2wCNbDS/+sURWP1Oi1lsTKW3ZqT",
+
+        // Supersphincs public key of the client base-64 encoded
+        "publicKey": "VvYTfCAhiEDW3abLLhO2ane27HMivnNSLjfKxd4jnOiGCOW0UEXjjacgoZrn/BPvNv+bmerLr0HB+71X2+Eh5NXH2JO6kAoM+SCQblUk3gDyqRbVbYkg/RSCl/6oe0wY"
+    },
+    "sessions": {
+        // Client IP
+        "ip": "192.168.0.3",
+
+        // The Agent ID of the App
+        "agent": "vwoA1JzkT6d7SXjIBoZ2egYlSn6Ajzge",
+
+        // Supersphincs public key of the client base-64 encoded
+        "publicKey": "rPyoqSZrNNUVpjKdhGLDD4sjXd8lgIgnRBY2NP5n8PDDLSvoLoD5n4GjaxbAfSDjagBjN8zztUQTNG1EKO9IgpgTLkfkTkhWqdgkC/K3EQLh6AMCZ8snlnles2QrbHAy"
+    }
+}
+```
+
+Then you can require the module and load it in your express app:
+
+```javascript
+const express = require( 'express' );
+const app = express();
+require( 'microtunnel-server' )( app,
+    {
+        appCredFile: 'appCred.json',
+        authClientsFile: 'authClients.json'
+    }
+);
+
+// This is a standard express GET route
+app.get( '/page/:id', ( req, res ) => {
+    const pageId = req.params.id;
+    res.json( `Received pageId: ${pageId} via public route` );
+} );
+
+// This is an authenticated express GET route for every client in authClients.json
+app.authGet( true, '/page/:id', ( req, res ) => {
+    const pageId = req.params.id;
+    res.json( `Received pageId: ${pageId} via autheticated route from ${req.tunnelClt.name}` );
+} );
+
+// This is an authenticated express POST route limited to 'frontEnd' client in authClients.json
+app.authPost( 'frontEnd', '/another-route', ( req, res ) => {
+    const data = req.body;
+    res.json( { receivedData: data } );
+} )
+
+app.listen( 3000, '0.0.0.0' );
+```
+
+## Configuration
+
+### `require( 'microtunnel-server' )( app, options )`
+
+#### Paramaters
+
+* `app` Required - An Express app/router instance
+* `options` Optional - An object containing custom configuration:
+  * `api` Optional - Root route for microtunnel (note: must be the same for clients) - Default `'/microtunnel'`
+  * `resetMinutes` Optional - Max duration for any session in minutes - Default `15`
+  * `appCredFile` Optional - Relative path of the credentials file - Default: enviroment var `APP_CRED`
+  * `authClientsFile` Optional - Relative path of the autherized clients file - Default: enviroment var `AUTH_CLTS`
+
+## Methods
+
+### `app.authGet`
+```javascript
+app.authGet( clientName: String | Array | true, route: String, callback: Function [, ...callback: Function] )
+```
+
+#### Parameters
+* `clientName` Required - To grant access to route only for authorized clients you can set it as a string containing client name or as an array of these. Otherwise you can authorize every clients setting it to `true`
+* `route` Required - The path for which the middleware function is invoked in Express-style
+* `callback` Required - Callback functions that accept `req`, `res` and `next`
+
+### `app.authPost`
+```javascript
+app.authPost( clientName: String | Array | true, route: String, callback: Function [, ...callback: Function] )
+```
+
+#### Parameters
+* `clientName` Required - To grant access to route only for authorized clients you can set it as a string containing client name or as an array of these. Otherwise you can authorize every clients setting it to `true`
+* `route` Required - The path for which the middleware function is invoked in Express-style
+* `callback` Required - Callback functions that accept `req`, `res` and `next`
+
+## Note
+* Request body will always be decrypted for POST method
+* You can call `req.tunnelClt` to show current client properties
+* Both `res.send` and `res.json` will decrypt data sent to clients but `microtunnel-client` auto-parses from JSON so use `res.json`
+* Since every communication is encrypted you can call `res.json` once for each request
+* `app.use` will not affect microtunnel routes
---
+++ index.js
@@ -0,0 +1,288 @@
+"use strict";
+const { superSphincs } = require( 'supersphincs' ),
+    { kyber } = require( 'kyber-crystals' ),
+    symCryptor = require( 'symcryptor' );
+
+if ( process.argv[1] === __filename && process.argv[2] === 'cred-generate' ) {
+    Promise.all( [
+        superSphincs.keyPair(),
+        symCryptor.rndBytes( 24 )
+    ] )
+    .then( res => {
+        const cred = {
+            publicKey: Buffer.from( res[0].publicKey ).toString( 'base64' ),
+            privateKey: Buffer.from( res[0].privateKey ).toString( 'base64' ),
+            agent: res[1].toString( 'base64' )
+        }
+        console.log( JSON.stringify( cred, null, 4 ) );
+    } )
+    .catch( console.error )
+    .finally( process.exit );
+} else {
+    const octetParser = require( 'express' ).raw( {
+        inflate: false,
+        limit: '1mb',
+        type: 'application/octet-stream'
+    } );
+
+    const auth1Len = 64;
+    const auth2Len = 31554;
+
+    const servAuth = ( app, options = {} ) => {
+        const defaultOptions = {
+            api: '/microtunnel',
+            resetMinutes: 15,
+            appCredFile: process.env.APP_CRED,
+            authClientsFile: process.env.AUTH_CLTS,
+            ...options
+        };
+        const { api, resetMinutes, appCredFile, authClientsFile } = defaultOptions;
+        class AuthServerSex {
+            constructor( name, ip, agent, signature ) {
+                this.name = name;
+                this.ip = ip;
+                this.agent = agent;
+                this.signature = signature;
+                this.state = 0;
+                this.key = undefined;
+                this.shaKey = undefined;
+                this.timer = setInterval( () => {
+                    this.delete();
+                }, resetMinutes * 60000 );
+            }
+            delete() {
+                this.state = 0;
+                this.key = undefined;
+                this.shaKey = undefined;
+            }
+            extract() {
+                const obj = {
+                    name: this.name,
+                    ip: this.ip,
+                    agent: this.agent,
+                    signature: this.signature,
+                    state: this.state,
+                    key: this.key,
+                    shaKey: this.shaKey
+                };
+                return obj;
+            }
+            resetTimer() {
+                clearInterval( this.timer );
+                this.timer = setInterval( () => {
+                    this.delete();
+                }, resetMinutes * 60000 );
+            }
+            reset() {
+                this.resetTimer();
+                this.delete();
+            }
+        }
+    
+        class AuthServerSessions extends Array {
+            constructor( authClients ) {
+                const clients = [];
+                for ( const clt in authClients ) {
+                    clients.push( new AuthServerSex( clt, authClients[clt].ip, authClients[clt].agent, Buffer.from( authClients[clt].publicKey, 'base64' ) ) );
+                }
+                super( ...clients );
+            }
+            get( str ) {
+                if ( !str || typeof str !== 'string' ) return false;
+                const found = this.find( el => el.name === str );
+                if ( found ) return found;
+                return false;
+            }
+            extract( str ) {
+                const found = this.get( str );
+                if ( !found ) return found;
+                return found.extract();
+            }
+            resetTimer( str ) {
+                const found = this.get( str );
+                if ( !found ) return found;
+                return found.resetTimer();
+            }
+            reset( str ) {
+                const found = this.get( str );
+                if ( !found ) return found;
+                return found.reset();
+            }
+            findSrv( ip, agent ) {
+                if (
+                    ( !ip || typeof ip !== 'string' )
+                    || ( !agent || typeof agent !== 'string' )
+                ) return false;
+                const found = this.find( el => {
+                    return ( el.ip === ip && el.agent === agent ) ? true : false;
+                } );
+                if ( found ) return found;
+                return false;
+            }
+        }
+        const sendError = function () {
+            this.status( 404 );
+            this.send();
+        };
+        const sessions = new AuthServerSessions( require( './' + authClientsFile ) );
+        const appCred = require( './' + appCredFile );
+        const unless = ( middleware ) => {
+            return ( req, res, next ) => {
+                if ( req.originalUrl.startsWith( api + '/' ) ) {
+                    return next();
+                } else {
+                    return middleware( req, res, next );
+                }
+            };
+        };
+        const origUse = app.use;
+        app.use = function ( ...callbacks ) {
+            if ( !callbacks.length ) throw new Error( '.use() method requires at least one function' );
+            if ( typeof callbacks[0] ==='string' || callbacks[0].constructor === RegExp || callbacks[0].constructor === Array ) {
+                if ( !( callbacks.length -1 ) ) throw new Error( '.use() method requires at least one function' );
+                const route = callbacks.shift();
+                for ( let i = 0; i < callbacks.length; i++ ) {
+                    origUse.call( this, route, unless( callbacks[i] ) );
+                }
+            } else {
+                for ( let i = 0; i < callbacks.length; i++ ) {
+                    origUse.call( this, unless( callbacks[i] ) );
+                }
+            }
+        };
+        app.post( api +'/auth1', octetParser, ( req, res ) => {
+            const ip = req.ip;
+            const agent = req.get( 'User-Agent' );
+            if ( !ip || !agent ) return sendError.call( res );
+            const clt = sessions.findSrv( ip, agent );
+            if ( !clt ) return sendError.call( res );
+            clt.reset();
+            if ( parseInt( req.headers['content-length'], 10 ) !== auth1Len ) return sendError.call( res );
+            Promise.all( [
+                kyber.keyPair(),
+                symCryptor.rndBytes( 64 ),
+                superSphincs.signDetached( req.body, Buffer.from( appCred.privateKey, 'base64' ), appCred.agent )
+            ] )
+            .then( result => {
+                clt.key = result[0].privateKey;
+                clt.shaKey = result[1];
+                const ret = Buffer.concat( [result[0].publicKey, Buffer.from( result[2] ), result[1]] );
+                clt.state = 1;
+                res.set( {
+                    'Content-Type': 'application/octet-stream'
+                } );
+                res.send( ret );
+                return res.end();
+            } )
+            .catch( () => sendError.call( res ) );
+        } );
+
+        app.post( api +'/auth2', octetParser, ( req, res ) => {
+            const ip = req.ip;
+            const agent = req.get( 'User-Agent' );
+            if ( !ip || !agent ) return sendError.call( res );
+            const clt = sessions.findSrv( ip, agent );
+            if ( !clt ) {
+                return sendError.call( res );
+            } else if ( clt.state !== 1 ) {
+                clt.reset();
+                return sendError.call( res );
+            }
+            if ( parseInt( req.headers['content-length'], 10 ) !== auth2Len ) return sendError.call( res );
+            const data = new Uint8Array( req.body );
+            const ciphertext = data.slice( 0, 1568 ),
+                encShaKey = data.slice( 1568, 1616 ),
+                encSignedRnd = data.slice( 1616 );
+            kyber.decrypt( ciphertext, clt.key )
+            .then( async decryptedKey => {
+                const shaKey = await symCryptor.decrypt( encShaKey, decryptedKey );
+                const signToCheck = new Uint8Array( await symCryptor.decrypt( encSignedRnd, decryptedKey, shaKey, clt.agent ) );
+                const verySign = await superSphincs.verifyDetached( signToCheck, clt.shaKey, clt.signature, shaKey );
+                if ( !verySign ) throw new Error( 'Internal server error' );
+                clt.key = decryptedKey;
+                clt.shaKey = shaKey;
+                const confirmation = await symCryptor.encrypt(
+                    Buffer.from( 'true' ),
+                    decryptedKey,
+                    shaKey,
+                    appCred.agent
+                );
+                res.send( confirmation );
+                clt.state = 2;
+                res.set( {
+                    'Content-Type': 'application/octet-stream'
+                } );
+                return res.end();
+            } )
+            .catch( () => sendError.call( res ) );
+        } );
+
+        const clientParser = ( clts, parseBody = false ) => {
+            return ( req, res, next ) => {
+                const ip = req.ip;
+                const agent = req.get( 'User-Agent' );
+                if ( !ip || !agent ) return sendError.call( res );
+                const clt = clts.find( el => 
+                    el.ip === ip
+                    && el.agent === agent
+                    && el.state === 2
+                );
+                if ( !clt ) return sendError.call( res );
+                req.tunnelClt = clt;
+                const origSend = res.send;
+                res.send = async function ( obj ) {
+                    try {
+                        const encrypted = await symCryptor.encrypt( Buffer.from( obj ), clt.key, clt.shaKey, appCred.agent );
+                        origSend.call( this, encrypted );
+                        res.end();
+                    } catch {
+                        res.status( 400 );
+                        res.end();
+                    }
+                };
+                res.set( {
+                    'Content-Type': 'application/octet-stream'
+                } );
+                if ( !parseBody ) return next();
+                symCryptor.decrypt( req.body, clt.key, clt.shaKey, clt.agent )
+                .then( async decBody => {
+                    req.body = JSON.parse( decBody.toString( 'utf8' ) );
+                    next();
+                } )
+                .catch( () => sendError.call( res ) );
+            };
+        };
+        const addAppCltRoute = ( cltNames, method, route = '/', ...callbacks ) => {
+            const clts = [];
+            if ( typeof cltNames === 'string' ) {
+                clts.push( sessions.get( cltNames ) );
+            } else if ( cltNames.constructor === Array ) {
+                for ( let clt of cltNames ) {
+                    if ( !clts.find( el => el.name === clt ) ) clts.push( sessions.get( clt ) );
+                }
+            } else if ( cltNames === true ) {
+                for ( let clt of sessions ) {
+                    clts.push( clt );
+                }
+            }
+            if ( !clts.length ) throw new Error( 'Invalid server name' );
+            if ( !clts.every( a => a ) ) throw new Error( 'Invalid server name' );
+            if ( !callbacks.length ) throw new Error( 'Callbacks argument requires at least one function' );
+            if ( method === 'get' ) {
+                app.get( api + route, clientParser( clts ), ...callbacks );
+            } else if ( method === 'post' ) {
+                app.post( api + route, octetParser, clientParser( clts, true ), ...callbacks );
+            } else {
+                throw new Error( 'Invalid route method' );
+            }
+        };
+        app.authGet = function ( clientName, route, ...callbacks ) {
+            addAppCltRoute( clientName, 'get', route, ...callbacks );
+        }
+        app.authPost = function ( clientName, route, ...callbacks ) {
+            addAppCltRoute( clientName, 'post', route, ...callbacks );
+        }
+    };
+
+    module.exports = servAuth;
+}
